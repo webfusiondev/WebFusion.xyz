d8105e4eed991fb5c5ee398dc3fd6a1344fe76d0
jbrit <pro.ajibolaojo@gmail.com>
2024-06-26 03:56:58 +0100

    events update

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,4 +1,6 @@
+import type { InferGetStaticPropsType, GetStaticProps } from "next";
 import { ki } from "@/app/fonts";
+import { promises as fs } from "fs";
 import Nav from "@/app/nav";
 import "../app/globals.css";
 import Footer from "@/app/footer";
@@ -9,37 +11,36 @@ import { useQueryParamState } from "@/hooks";
 import moment from "moment";
 import Link from "next/link";
 
-// TODO: validators
-export default function Courses() {
-  const allEvents: Array<{
+type AllEvents = Array<{
+  name: string;
+  events: {
     name: string;
-    events: {
-      name: string;
-      region: string;
-      date: `${number}/${number}/${number}`;
-      tag: string;
-      attendance: string;
-      link: string;
-    }[];
-  }> = [
-    {
-      name: "Upcoming Events",
-      events: [
-        {
-          name: "Buidl for Africa Hackathon",
-          date: "07/01/2024",
-          region: "Nigeria",
-          tag: "Hackathons",
-          attendance: "Hybrid",
-          link: "#",
-        },
-      ],
-    },
-    {
-      name: "Past Events",
-      events: [],
-    },
-  ];
+    region: string;
+    date: `${number}/${number}/${number}`;
+    time: string;
+    tag: string;
+    attendance: string;
+    link: string;
+  }[];
+}>;
+
+export const getStaticProps = (async (context) => {
+  const file = await fs.readFile(
+    process.cwd() + "/resources/events.json",
+    "utf8"
+  );
+
+  let allEvents: AllEvents = JSON.parse(file);
+
+  return { props: { allEvents } };
+}) satisfies GetStaticProps<{
+  allEvents: AllEvents;
+}>;
+
+// TODO: validators
+export default function Events({
+  allEvents,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   const attendanceFilters = [
     { name: "Remote & IRL" },
     { name: "Hybrid" },
@@ -81,8 +82,12 @@ export default function Courses() {
     events: events.filter(
       (event) =>
         [typeFilters[0].name, event.tag].includes(selectedTypeFilter.name) &&
-        [regionFilters[0].name, event.region].includes(selectedRegionFilter.name) &&
-        [attendanceFilters[0].name, event.attendance].includes(selectedAttendanceFilter.name)
+        [regionFilters[0].name, event.region].includes(
+          selectedRegionFilter.name
+        ) &&
+        [attendanceFilters[0].name, event.attendance].includes(
+          selectedAttendanceFilter.name
+        )
     ),
   }));
   return (
@@ -156,7 +161,7 @@ export default function Courses() {
                   ) : (
                     events.map((event) => (
                       <div
-                        key={event.name + event.date + event.tag}
+                        key={event.name + event.date + event.tag + event.time}
                         className="px-6 py-5 bg-[rgba(150,_150,_150,_0.05)]"
                       >
                         <div className="font-bold text-lg md:text-xl xl:text-2xl mb-2">
@@ -232,6 +237,35 @@ export default function Courses() {
                         </div>
                         <div className="flex mb-2">
                           <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 32 32"
+                            id="clock"
+                            height="22px"
+                            width="22px"
+                            className="mr-3"
+                          >
+                            <circle
+                              cx="16"
+                              cy="16"
+                              r="13.5"
+                              fill="#fff"
+                            ></circle>
+                            <circle
+                              cx="16"
+                              cy="16"
+                              r="1.5"
+                              fill="#e5303e"
+                            ></circle>
+                            <path
+                              fill="#e5303e"
+                              d="M16,.5A15.5,15.5,0,1,0,31.5,16,15.5,15.5,0,0,0,16,.5Zm0,28A12.5,12.5,0,1,1,28.5,16,12.5,12.5,0,0,1,16,28.5Z"
+                            ></path>
+                            <path d="M21.5,15.5H17.4079a1.49809,1.49809,0,0,0-.9079-.9079V8.5a.5.5,0,0,0-1,0v6.0921A1.4963,1.4963,0,1,0,17.4079,16.5H21.5a.5.5,0,0,0,0-1Zm-5.5,1a.5.5,0,1,1,.5-.5A.50034.50034,0,0,1,16,16.5Zm9.21417-9.65955c-.00873-.01025-.01208-.02313-.02179-.03284s-.02258-.01306-.03284-.02179a12.97336,12.97336,0,0,0-18.31909,0c-.01025.00873-.02313.01208-.03284.02179s-.01306.02258-.02179.03284a12.97336,12.97336,0,0,0,0,18.31909c.00873.01025.01208.02313.02179.03284s.02258.01306.03284.02179a12.97336,12.97336,0,0,0,18.31909,0c.01025-.00873.02313-.01208.03284-.02179s.01306-.02258.02179-.03284a12.97336,12.97336,0,0,0,0-18.31909ZM24.81793,24.1109l-1.03961-1.03961a.49995.49995,0,0,0-.707.707l1.03961,1.03961A11.93609,11.93609,0,0,1,16.5,27.97467V26.5a.5.5,0,0,0-1,0v1.47467a11.93609,11.93609,0,0,1-7.6109-3.15674l1.03961-1.03961a.49995.49995,0,0,0-.707-.707L7.18207,24.1109A11.93609,11.93609,0,0,1,4.02533,16.5H5.5a.5.5,0,0,0,0-1H4.02533A11.93609,11.93609,0,0,1,7.18207,7.8891L8.22168,8.92871a.49995.49995,0,0,0,.707-.707L7.8891,7.18207A11.93609,11.93609,0,0,1,15.5,4.02533V5.5a.5.5,0,0,0,1,0V4.02533a11.93609,11.93609,0,0,1,7.6109,3.15674L23.07129,8.22168a.49995.49995,0,1,0,.707.707L24.81793,7.8891A11.93609,11.93609,0,0,1,27.97467,15.5H26.5a.5.5,0,0,0,0,1h1.47467A11.93609,11.93609,0,0,1,24.81793,24.1109ZM16,0A16,16,0,1,0,32,16,16.01833,16.01833,0,0,0,16,0Zm0,31A15,15,0,1,1,31,16,15.017,15.017,0,0,1,16,31Z"></path>
+                          </svg>
+                          <span>{event.time}</span>
+                        </div>
+                        <div className="flex mb-2">
+                          <svg
                             className="mr-3"
                             xmlns="http://www.w3.org/2000/svg"
                             xmlnsXlink="http://www.w3.org/1999/xlink"
@@ -276,13 +310,19 @@ export default function Courses() {
                               />
                             </g>
                           </svg>
-                          <span>{event.region} ({event.attendance})</span>
+                          <span>
+                            {event.region} ({event.attendance})
+                          </span>
                         </div>
                         <div className="flex items-end">
                           <span className="text-sm rounded-sm bg- border border-dashed px-1 text-left inline-block">
                             {event.tag}
                           </span>
-                          <Link className="inline-block ml-auto" href={event.link}>
+                          <Link
+                            className="inline-block ml-auto"
+                            target="_blank"
+                            href={event.link}
+                          >
                             <button className=" bg-brand-green text-brand-black px-3 py-2 lg:text-lg">
                               View More
                             </button>
